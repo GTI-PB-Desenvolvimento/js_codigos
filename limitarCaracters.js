4f9fcb31dd838847a3989e9ed871f419f1b1c079
Pedro Henrick <81793399+pedrohenrick777@users.noreply.github.com>
2021-11-12 11:03:15 -0300

    Create limitarCaracters.js

---
+++ limitarCaracters.js
@@ -0,0 +1,21 @@
+//Função para impedir que o usuário digite um valor indesejado.
+//Para usar, basta importar este arquivo no Html e adicionar um "onKeypress = limitcarc(event, TIPO)" ao input desejado.
+//O parâmetro TIPO deve ser preenchido com:
+
+//letr_acent - Para letras com acentos
+//num - Para números
+//letr - Para letras sem acentos
+
+function limitcarc(e, tipo) {
+    var chr = String.fromCharCode(e.which);
+    if (tipo == 'letr_acent'){
+        if ("áéíóúâêîôûãõ qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".indexOf(chr) < 0)
+          return false;
+    }else if (tipo == 'num') {
+        if ("0123456789".indexOf(chr) < 0)
+          return false;
+    }else if (tipo == 'letr'){
+        if ("qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".indexOf(chr) < 0)
+          return false;
+    }
+}
